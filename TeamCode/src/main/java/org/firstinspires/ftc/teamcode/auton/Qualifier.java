6c8fb20aa60133a43fa94e363db512ec1c8ffe95
WillowInTheWind <123593045+WillowInTheWind@users.noreply.github.com>
2023-02-02 16:50:10 -0600

    Backup 1/25

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/Qualifier.java
@@ -0,0 +1,381 @@
+/*
+ * Copyright (c) 2021 OpenFTC Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.AprilTagDetectionPipeline;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+@Autonomous(name = "Meet 3 Auton Left")
+public class Qualifier extends LinearOpMode
+{
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+    // Tag ID 1,2,3 from the 36h11 family
+    int left = 1;
+    int middle = 2;
+    int right = 3;
+    AprilTagDetection tagOfInterest = null;
+
+    private DcMotor backright;
+    private DcMotor frontright;
+    private DcMotor backleft;
+    private DcMotor frontleft;
+    private DcMotor slide;
+
+    private Servo leftClaw;
+    private Servo rightClaw;
+
+    String AutonDirection = "";
+
+    @Override
+    public void runOpMode()
+    {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
+        backright = hardwareMap.get(DcMotor.class, "back right");
+        frontright = hardwareMap.get(DcMotor.class, "front right");
+        backleft = hardwareMap.get(DcMotor.class, "back left");
+        frontleft = hardwareMap.get(DcMotor.class, "front left");
+        slide = hardwareMap.get (DcMotor.class, "slide");
+
+        leftClaw = hardwareMap.get(Servo.class, "left claw");
+        rightClaw = hardwareMap.get(Servo.class, "right claw");
+
+        backright.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontright.setDirection(DcMotorSimple.Direction.REVERSE);
+        slide.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        backright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.log().add("This was built");
+
+        closeClaw();
+
+        while (!gamepad1.right_bumper&& !gamepad1.left_bumper) {
+            if (gamepad1.left_bumper){
+                AutonDirection = "left";
+                telemetry.addLine("Auton Mode: Left");
+            }
+            if (gamepad1.right_bumper) {
+                AutonDirection = "Right";
+                telemetry.addLine("Auton Mode: Right");
+            }
+        }
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == left || tag.id == middle|| tag.id == right)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                }
+                else
+                {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if(tagOfInterest == null)
+                    {
+                        telemetry.addLine("(The tag has never been seen)");
+                    }
+                    else
+                    {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+
+            }
+            else
+            {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if(tagOfInterest == null)
+                {
+                    telemetry.addLine("(The tag has never been seen)");
+                }
+                else
+                {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+
+            }
+
+            telemetry.addData("Slide Position", slide.getCurrentPosition());
+            telemetry.update();
+            sleep(20);
+        }
+
+        /*
+         * The START command just came in: now work off the latest snapshot acquired
+         * during the init loop.
+         */
+
+        /* Update the telemetry */
+        if(tagOfInterest != null)
+        {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        }
+        else
+        {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+
+        if (AutonDirection == "left") {
+            //Raise up to avoid cone interference
+            moveSlide(200);
+            //align bot
+            Drive(150, .4, Math.toRadians(0));
+            //drive forward
+            Drive(1550,.4,Math.toRadians(90));
+            sleep(250);
+            Drive(450,.4,Math.toRadians(270));
+            //Strafe RIGHT to middle junction
+            Drive(600, .4, Math.toRadians(0));
+            //Raise slide to middle junction
+            moveSlide(3000);
+            sleep(250);
+            //aproach middle junction
+            Drive(200,.4,Math.toRadians(90));
+            sleep(500);
+            //drop cone
+            openClaw();
+            sleep(500);
+            //backup
+            Drive(150,.4,Math.toRadians(270));
+            closeClaw();
+            //lower slide
+            moveSlide(0);
+            sleep(500);
+            //go to parking spot
+            if(tagOfInterest == null ||tagOfInterest.id == middle) {
+                //trajectory
+                Drive(600,.4,Math.toRadians(180));
+                sleep(1000);
+            }
+            else if(tagOfInterest.id == right) {
+                //trajectory
+                Drive(600,.4,Math.toRadians(0));
+                sleep(1000);
+            }
+
+            else if(tagOfInterest.id == left) {
+                //trajectory
+                Drive(1800, .4, Math.toRadians(180));
+                sleep(1000);
+            }
+        }
+        else if (AutonDirection == "Right") {
+            //Raise up to avoid cone interference
+            moveSlide(200);
+            //align bot
+            Drive(150, .4, Math.toRadians(180));
+            sleep(250);
+            //drive forward
+            Drive(1650,.4,Math.toRadians(90));
+            sleep(250);
+            Drive(450,.4,Math.toRadians(270));
+            sleep(250);
+            //Strafe Left to middle junction
+            Drive(625, .4, Math.toRadians(180));
+            //Raise slide to middle junction
+            moveSlide(3000);
+            sleep(250);
+            sleep(250);
+            //aproach middle junction
+            Drive(130,.4,Math.toRadians(90));
+            sleep(500);
+            //drop cone
+            openClaw();
+            sleep(500);
+            //backup
+            Drive(150,.4,Math.toRadians(270));
+            closeClaw();
+            //lower slide
+            moveSlide(0);
+            sleep(500);
+            //go to parking spot
+            if(tagOfInterest == null ||tagOfInterest.id == middle) {
+                //trajectory
+                Drive(600,.4,Math.toRadians(0));
+                sleep(1000);
+            }
+            else if(tagOfInterest.id == left) {
+                //trajectory
+                Drive(600,.4,Math.toRadians(180));
+                sleep(1000);
+            }
+
+            else if(tagOfInterest.id == right) {
+                //trajectory
+                Drive(1800, .4, Math.toRadians(0));
+                sleep(1000);
+            }
+        }
+
+
+    }
+    private void Drive(double position, double power, double angle) {
+
+        backleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        DriveAngle(angle, power);
+        while (Math.abs(backleft.getCurrentPosition())<Math.abs(position) && opModeIsActive()) {
+            telemetry.addData("Target Position", position);
+            telemetry.addData("Motor Position", backleft.getCurrentPosition());
+        }
+        stopRobot();
+    }
+
+    public void stopRobot(){
+        backleft.setPower(0);
+        backright.setPower(0);
+        frontleft.setPower(0);
+        frontright.setPower(0);
+
+
+    }
+    private void DriveAngle(double angle, double speed) {
+        angle = angle - Math.toRadians(45);
+        frontleft.setPower(speed * Math.cos(angle));
+        backright.setPower(speed * Math.cos(angle));
+        backleft.setPower(speed * Math.sin(angle));
+        frontright.setPower(speed * Math.sin(angle));
+    }
+    void tagToTelemetry(AprilTagDetection detection)
+    {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+
+    void closeClaw () {
+        leftClaw.setPosition(.1);
+        rightClaw.setPosition(.65);
+    }
+    void openClaw () {
+        leftClaw.setPosition(.25);
+        rightClaw.setPosition(.4);
+    }
+    void moveSlide(int position) {
+        moveSlide(position, 1, true);
+    }
+    void moveSlide(int position,double power) {
+        moveSlide(position, power, true);
+    }
+    void moveSlide (int position,double power, boolean waitForCompletion) {
+        slide.setTargetPosition(position);
+        slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slide.setPower(power);
+        if (waitForCompletion) {
+            while (slide.isBusy()) {
+                telemetry.addData("SlidePosition", slide.getCurrentPosition());
+                telemetry.update();
+            }
+        }
+    }
+}
